1a760f1298c80d31cece80888cdb73f6f0cbae18
Your Name <a.bouargane@crmefsm.ac.ma>
2025-02-17 11:50:59 +0100

    Ajout de la gestion du port pour Render

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+import os
 from fastapi import FastAPI
 from backend.database import engine, Base
 from backend.routes import router
 
 app = FastAPI()
 
-# Création automatique des tables
 Base.metadata.create_all(bind=engine)
-
 app.include_router(router)
 
 @app.get("/")
 def home():
     return {"message": "API fonctionne"}
 
+# Forcer l'usage du bon port
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
